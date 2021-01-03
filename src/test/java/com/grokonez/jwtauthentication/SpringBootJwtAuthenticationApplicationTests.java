5e946ff907823d1531cf6dfc2add776187270a99
Amchayd Mariam <meriam.amchayd@gmail.com>
2021-01-03 11:53:52 +0100

    Authentification spring boot && spring security JWT && MySQL

---
+++ src/test/java/com/grokonez/jwtauthentication/SpringBootJwtAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grokonez.jwtauthentication;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+//@RunWith(SpringRunner.class)
+public class SpringBootJwtAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
